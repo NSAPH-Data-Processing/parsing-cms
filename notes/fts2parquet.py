1e3a43d22b8c511aaa59e1f44bbacc804fdc9cd0
James Kitch <jkitch31@gmail.com>
2024-09-11 10:38:38 -0400

    working start to fts2parquet.py

--- notes/fts2parquet.py
+++ notes/fts2parquet.py
@@ -1,8 +1,11 @@
-import pandas as pd
-import os
 import argparse
-import math
+#import glob
+import pandas as pd
+import numpy as np
+#import datetime
+import os
 from typing import List, Tuple, Any, Callable
+from itertools import accumulate
 
 from dorieh.cms.fts2yaml import mcr_type, MedicareFTS
 from dorieh.platform.loader.data_loader import DataLoader
@@ -12,20 +15,41 @@ from dorieh.utils.fwf import FWFReader
 from dorieh.utils.io_utils import fopen
 
 
+# fast line parser for fixed-width files
+def fwf_parser(fieldwidths):
+    # get absolute cut points for columns
+    cuts = tuple(cut for cut in accumulate(abs(fw) for fw in fieldwidths))
+    # turn into tuples
+    flds = tuple(zip((0,)+cuts, cuts))
+    # format into string and pass to eval() for lazy evaluation
+    slcs = ', '.join(f'line[{i}:{j}]' for i, j in flds)
+    parse = eval('lambda line: ({})\n'.format(slcs))  # Create and compile source code.
+    return parse
+
+def change_dftypes(df, type_dict, verbose=False):
+    for col in df.columns:
+        if type_dict[col] == 'NUM':
+            if verbose: 
+                print(col + ": CHAR --> NUM")
+            df[col] = pd.to_numeric(df[col].str.strip(), errors='coerce')
+        elif type_dict[col] == 'DATE':
+            if verbose: 
+                print(col + ": CHAR --> DATE")
+            df[col] = pd.to_datetime(df['COVSTART'], errors='coerce')
+        else:
+            df[col] = df[col].str.strip().replace('', np.nan)
+            if verbose: 
+                print(col + ": CHAR")
+    return(df)
+
 # takes fts path and converts .dat to parquet
-def fts2parquet(fts_path, parquet_path=None, max_rows = None, verbose=True):
+def fts2parquet(fts_path, parquet_path=None, max_rows = None, row_step = 10**6, verbose=True):
 
     # extracting basic information about file
     f, _ = os.path.splitext(fts_path)
     _, fname = os.path.split(f)
     t = mcr_type(fname)
     dat_path = f + ".dat"
-
-    if max_rows is None:
-        max_rows = math.inf
-    else:
-        max_rows = int(max_rows)
-
     if parquet_path is None:
         parquet_path = f + ".parquet"
 
@@ -35,42 +59,78 @@ def fts2parquet(fts_path, parquet_path=None, max_rows = None, verbose=True):
     # getting column names
     fts_meta = fts.to_fwf_meta(dat_path)
     colnm_lst = [col.name for col in fts_meta.columns]
+    colwidth_lst = [col.length for col in fts_meta.columns]
+    #colspecs = [(col.start, col.end) for col in fts_meta.columns]
+    type_dict = {col.name: col.type for col in fts_meta.columns}
+    parse = fwf_parser(colwidth_lst)
 
-    # reading body of .dat file, one row at a time
-    with FWFReader(fts_meta) as reader:
-        # this gives us one row at a time
-        dat = []
-        record_count = 0 
-        for record in reader:
-            dat.append(record)
-            record_count += 1 
-            # optional printing of progress
-            if verbose and record_count % 5000000 == 0:
-                print(record_count)
+    # read raw fixed width file
+    print("Reading raw fw file " + dat_path + "...")
+    with open(dat_path, "r") as f:
+        lines = f.read().split("\n")
+        f.close()
 
-            # stop loading data if have reached max rows
-            if record_count >= max_rows:
-                break
+    if max_rows is not None:
+        max_rows = min(max_rows, len(lines))
+    else:
+        max_rows = len(lines)
+
+    row_min = 0
+    row_max = row_step
+    parsed_df_lst = []
+    while row_min < max_rows:
+        if row_max >= max_rows: 
+            row_max = max_rows
+        print("Processing rows " + str(row_min) + " to " + str(row_max))
+        # parsing raw text into columns
+        prs_df_tmp = pd.DataFrame([parse(line) for line in lines[row_min:row_max]], 
+                                  columns=colnm_lst)
+        # modifying data frame types
+        parsed_df_lst.append(change_dftypes(prs_df_tmp, type_dict=type_dict, verbose=False))
+        row_min = row_max
+        row_max += row_step
     
-        reader.close()
+    # concatenating all dataframes
+    print("Concatenating data frames")
+    full_df = pd.concat(parsed_df_lst)
+    
+    # printing number of NAs in every columns
+    print("NA summary statistics")
+    print(full_df.isnull().sum())
+
 
     # turning list into data frame object and exporting to parquet
-    print("List --> DataFrame")
-    df_out = pd.DataFrame(df_out, columns = colnm_lst)
-    df_out.to_parquet(parquet_path)
+    print("DF --> parquet")
+    full_df.to_parquet(parquet_path)
 
     return None
+
+
+def main(args):
+    pd.set_option('future.no_silent_downcasting', True)
+    if args.max_rows is not None:
+        max_rows = int(args.max_rows)
+    if args.row_step is not None:
+        row_step = int(args.row_step)
+
+    fts2parquet(fts_path=args.fts_path,
+                parquet_path=args.parquet_path,
+                max_rows = max_rows,
+                row_step = row_step,
+                verbose = args.verbose)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Takes .fts and .dat files and produces a .parquet file')
     parser.add_argument('fts_path', type=str, help='Name of spaceenv to be used')
     parser.add_argument('--parquet_path', type=str, default=None, help='[optional] parquet path')
+    parser.add_argument('--row_step', type=int, default=None, help='[optional] Maximum number of rows to read in')
     parser.add_argument('--max_rows', type=int, default=None, help='[optional] Maximum number of rows to read in')
     parser.add_argument('--verbose', type=bool, default=True, help='[optional] Log progress of loading .dat file')
     args = parser.parse_args()
 
-    fts2parquet(fts_path=args.fts_path,
-                parquet_path=args.parquet_path,
-                max_rows = args.max_rows,
-                verbose = args.verbose)
+    main(args)
+
+
+# python3 notes/fts2parquet.py "/n/dominici_nsaph_l3/Lab/data/ci3_d_medicare/original_data/cms_medicare/data/4334/2012/mbsf_ab_summary_res000017155_req004334_2012.fts" \
+#    --parquet_path "data/tmp.parquet" --row_step 1000000 --max_rows 10000000 --verbose "False"
